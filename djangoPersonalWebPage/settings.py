41c2e46ed4dec3fcc043b1cece2cbb788b524052
unknown <astrupnate@gmail.com>
2019-11-19 14:34:52 -0500

    heroku stuff

--- djangoPersonalWebPage/settings.py
+++ djangoPersonalWebPage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -144,3 +144,6 @@ STATICFILES_DIRS = [
 LOGIN_URL = 'myApp/userLogin'
 
 AUTH_USER_MODEL = 'myApp.Users'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
